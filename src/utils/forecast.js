a1d66ca48b4d62885a2f75d79db56f674a2b410a
Bruno Quagliata <bquagliata@effectussoftware.com>
2022-02-14 12:12:39 -0300

    Add humidity forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,14 +4,14 @@ const request = require('request')
 //Geocoding
 //Adress -> Lat/Long -> weather
 const forecast = (latitude, longitude, callback) =>{
-    const url = 'http://api.weatherstack.com/current?access_key=7ffe251d7e3ef5fe207bd0154c7c79f2&query=' + longitude + ','+ latitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=7ffe251d7e3ef5fe207bd0154c7c79f2&query=' + longitude + ','+ latitude + '&units=m'
     request({url, json: true}, (error, {body}) => {
         if (error){
             callback('Unable to connect to weather services!',undefined)
         } else if (body.error){
             callback('Unable to find location',undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently '  + body.current.temperature + ' degrees out. It feels like '+ body.current.feelslike +' 5 degrees out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently '  + body.current.temperature + ' degrees out. The high today is It feels like '+ body.current.feelslike +' 5 degrees out. The humidity is: '+ body.current.humidity + '%')
         }
     })
 }
